e2b3a5b640d78995de6377413a31c88d02697751
Fayadh <fayadh56@gmail.com>
2017-11-18 07:13:40 -0500

    Created Sign-Up Route

---
+++ chat-app/src/components/LandingPage.vue
@@ -0,0 +1,116 @@
+<template>
+ <div id="app">
+    <div class="body body-class">
+    <div id="#wrapper">
+    <div class = "tile is-ancestor main-tile">
+
+      <div class="tile is-ancestor">
+      <div class="tile is-8 is-vertical is-parent">
+        <div class="tile is-child">
+          <section class="hero is-primary hero-styles" id="#hero">
+          <div class="hero-body">
+            <div class="title-div">
+              <h1 class="title">
+                Mental Health Crisis Chat
+              </h1>
+              <h2 class="subtitle">
+                Lorem Ipsum
+              </h2>
+            </div>
+
+          </div>
+          </section>
+        </div>
+      </div>
+      <div class="tile is-parent">
+        <div class="tile is-child box">
+            <article class="tile is-child">
+              <div class="content login-box">
+                <div class="tile is-child is-vertical image-tile">
+                  <img src="https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-579281.jpg"/>
+                </div>
+                <div class="tile is-child box is-vertical">
+                  <b-field label="Username"
+                    type="is-success"
+                    message="This username is available">
+                    <b-input placeholder="Enter Username"></b-input>
+                  </b-field>
+
+                  <b-field label="Password"
+                    type="is-warning"
+                    :message="['Password is too short', 'Password must have at least 8 characters']">
+                  <b-input placeholder="Enter Password" type="password"></b-input>
+                  </b-field>
+
+                  <button class="button is-primary" @click="$toast.open('Login')">
+                        Login
+                  </button>
+
+                  <button class="button is-primary">
+                        <router-link to="/sign-up">Register</router-link>
+                  </button>
+                </div>
+              </div>
+            </article>
+        </div>
+      </div>
+    </div>
+    </div>
+  </div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LandingPage',
+  data () {
+    return {
+      msg: 'Welcome'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.body-class {
+    height: 100%;
+    min-height: 100%;
+    margin-left: 20px;
+    margin-right: 20px;
+    padding: 0;
+}
+
+.hero-styles {
+  background-image: linear-gradient(to top, #30cfd0 0%, #330867 100%);
+}
+
+#wrapper
+{
+    height: 100%;
+    min-height: 100%;
+}
+
+.hero-styles {
+  height: 90vh;
+}
+
+.main-tile {
+  height: 100%;
+  min-height: 100%;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+a {
+  color: #42b983;
+}
+
+.image-tile {
+  padding-top: 70px;
+}
+</style>
---
+++ chat-app/src/components/SignUp.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="sign-up">
+    <p> Register an Account Here!</p>
+    <input type="text" placeholder="email"><br>
+    <input type="password" placeholder="Password"></br>
+    <button>Sign Up</button>
+    <span>or go back to <router-link to="/">Login</router-link>.</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'signUp',
+    data: function() {
+      return {}
+    },
+    methods: {}
+  }
+</script>
+
+<style scoped>
+</style>
