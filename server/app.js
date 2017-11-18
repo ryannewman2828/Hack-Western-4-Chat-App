a557387f94c625a8614a47fed66fba6c56ccdf7d
Ryan Newman <ryannewman2828@gmail.com>
2017-11-17 23:25:19 -0500

    refactor backend

--- server/app.js
+++ server/app.js
@@ -5,25 +5,16 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./api/routes');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/api', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,7 +31,13 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+});
+
+const port = process.env.PORT || 9000;
+const env = process.env.NODE_ENV || 'development';
+
+app.listen(port, function() {
+  console.log('Listening on ' + port);
 });
 
 module.exports = app;
