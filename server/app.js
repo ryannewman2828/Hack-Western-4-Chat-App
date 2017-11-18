e392ffc8b0236e835b625bf0f50393284b366c5f
Ryan Newman <ryannewman2828@gmail.com>
2017-11-18 10:20:30 -0500

    comment out

--- server/app.js
+++ server/app.js
@@ -7,8 +7,13 @@ var bodyParser = require('body-parser');
 var admin = require("firebase-admin");
 
 // Fetch the service account key JSON file contents
+//var serviceAccount = require("path/to/serviceAccountKey.json");
 
 // Initialize the app with a service account, granting admin privileges
+//admin.initializeApp({
+//  credential: admin.credential.cert(serviceAccount),
+//  databaseURL: "https://databaseName.firebaseio.com"
+//});
 
 
 require('./models/db');
@@ -48,5 +53,10 @@ app.listen(port, function() {
 });
 
 // As an admin, the app has access to read and write all data, regardless of Security Rules
+//var db = admin.database();
+//var ref = db.ref("restricted_access/secret_document");
+//ref.once("value", function(snapshot) {
+//  console.log(snapshot.val());
+//});
 
 module.exports = app;
